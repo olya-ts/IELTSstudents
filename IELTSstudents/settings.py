57d203c3998c464ff2b16150ffe1d91f242898e2
olya-ts <tsizman.olga@mail.ru>
2022-02-09 14:47:13 +0500

    connected to MySQL'

--- IELTSstudents/settings.py
+++ IELTSstudents/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'IELTSstudents.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ieltsstudents',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '1223',
     }
 }
 
