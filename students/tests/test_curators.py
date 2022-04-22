72dc28bb756fca37cd9577d409fa04c86638cb34
olya-ts <tsizman.olga@mail.ru>
2022-04-22 13:34:52 +0400

    tests for Curator are completed

--- students/tests/test_curators.py
+++ students/tests/test_curators.py
@@ -1,6 +1,8 @@
 from rest_framework import status
-from rest_framework.test import APIClient
+from django.db.models import ProtectedError
 import pytest
+from model_bakery import baker
+from students.models import Curator, Student
 
 
 @pytest.fixture
@@ -11,12 +13,65 @@ def create_curator(api_client):
 
 
 @pytest.mark.django_db
-class TestGetCurator:
-    def test_if_user_is_anonymous_returns_401(self):
-        client = APIClient()
-        response = client.get('/ielts/curators/')
+class TestRetrieveCuratorList:
+    def test_if_user_is_anonymous_returns_401(self, api_client):
+        curators = baker.make(Curator, _quantity=10)
+
+        response = api_client.get('/ielts/curators/')
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_200(self, api_client, authenticate_user):
+        curators = baker.make(Curator, _quantity=10)
+
+        authenticate_user()
+        response = api_client.get('/ielts/curators/')
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_user_is_admin_returns_200(self, api_client, authenticate_user):
+        curators = baker.make(Curator, _quantity=10)
+
+        authenticate_user(is_staff=True)
+        response = api_client.get('/ielts/curators/')
+
+        assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+class TestRetrieveCurator:
+    def test_if_user_is_anonymous_returns_401(self, api_client):
+        curator = baker.make(Curator)
+
+        response = api_client.get(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_200(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user()
+        response = api_client.get(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': curator.pk,
+            'name': curator.name,
+            'phone': curator.phone
+        }
+
+    def test_if_user_is_admin_returns_200(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user(is_staff=True)
+        response = api_client.get(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': curator.pk,
+            'name': curator.name,
+            'phone': curator.phone
+        }
 
 
 @pytest.mark.django_db
@@ -48,19 +103,118 @@ class TestCreateCurator:
 
 
 @pytest.mark.django_db
-class TestPutCurator:
-    def test_if_user_is_anonymous_returns_401(self):
-        pass
+class TestModifyFullyCurator:
+    def test_if_user_is_anonymous_returns_401(self, api_client):
+        curator = baker.make(Curator)
+
+        response = api_client.put(f'/ielts/curators/{curator.pk}/', {'name': 'm', 'phone': '896512'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user()
+        response = api_client.put(f'/ielts/curators/{curator.pk}/', {'name': 'm', 'phone': '896512'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_admin_returns_200(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user(is_staff=True)
+        response = api_client.put(f'/ielts/curators/{curator.pk}/', {'name': 'm', 'phone': '896512'})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': curator.pk,
+            'name': 'm',
+            'phone': '896512'
+        }
 
 
 
 @pytest.mark.django_db
-class TestPatchCurator:
-    def test_if_user_is_anonymous_returns_401(self):
-        pass
+class TestModifyPartlyCurator:
+    def test_if_user_is_anonymous_returns_401(self, api_client):
+        curator = baker.make(Curator)
+
+        response = api_client.patch(f'/ielts/curators/{curator.pk}/', {'name': 'm'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user()
+        response = api_client.patch(f'/ielts/curators/{curator.pk}/', {'name': 'm'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_admin_returns_200(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user(is_staff=True)
+        response = api_client.patch(f'/ielts/curators/{curator.pk}/', {'name': 'm'})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': curator.pk,
+            'name': 'm',
+            'phone': curator.phone
+        }
 
 
 @pytest.mark.django_db
 class TestDeleteCurator:
-    def test_if_user_is_anonymous_returns_401(self):
-        pass
+    def test_if_user_is_anonymous_returns_401(self, api_client):
+        curator = baker.make(Curator)
+
+        response = api_client.delete(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user()
+        response = api_client.delete(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_admin_returns_204(self, api_client, authenticate_user):
+        curator = baker.make(Curator)
+
+        authenticate_user(is_staff=True)
+        response = api_client.delete(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+class TestDeleteRelatedToStudentCurator:
+    def test_if_user_is_anonymous_returns_401(self, api_client):
+        student = baker.make(Student)
+        curator = student.curator
+
+        response = api_client.delete(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, api_client, authenticate_user):
+        student = baker.make(Student)
+        curator = student.curator
+
+        authenticate_user()
+        response = api_client.delete(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_admin_returns_405(self, api_client, authenticate_user):
+        student = baker.make(Student)
+        curator = student.curator
+
+        authenticate_user(is_staff=True)
+        response = api_client.delete(f'/ielts/curators/{curator.pk}/')
+
+        assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
